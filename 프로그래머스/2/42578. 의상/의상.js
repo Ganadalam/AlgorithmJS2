1c9956ca275ff3b0c931108a6578654e38aa6cd4
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2026-01-20 21:29:19 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,29 +1,5 @@
-function solution(clothes) {
-  const map = new Map();
-
-  for (const [, category] of clothes) {
-    map.set(category, (map.get(category) || 0) + 1);
-  }
-
-  return [...map.values()].reduce((acc, cur) => acc * (cur + 1), 1) - 1;
+function solution(clothes){
+    const map = new Map();
+    for(const [,cat] of clothes) map.set(cat, (map.get(cat)||0)+1);
+    return [...map.values()].reduce((a,b)=>a*(b+1),1)-1;
 }
-// function solution(items) {
-//   const map = new Map();
-//   for (const [, category] of items) {
-//     map.set(category, (map.get(category) || 0) + 1);
-//   }
-//   return [...map.values()].reduce((acc, cur) => acc * (cur + 1), 1) - 1;
-// }
-
-
-
-// reduce() Map chaining , reduce() 곱셈, 체인 끝 -1 처리 => 시간복잡도: O(n)  
-// function solution(clothes) {
-//  return [...clothes.reduce((map, [, type]) =>
-//    map.set(type, (map.get(type) || 0) + 1), new Map()
-//  ).values()]
-//   .reduce((acc, count) => acc * (count + 1), 1) - 1;
-//}
-
-// 기존: O(n), 카테고리 수: for 반복 + 조건분기  
-// 곱셈: for.. of 순차 곱셈 ,반환: answer - 1
