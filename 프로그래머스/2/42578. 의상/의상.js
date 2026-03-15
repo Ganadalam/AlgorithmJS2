4dd3fa21a49ccec1d0d92a53fce10004de878209
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2026-03-15 13:11:44 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,5 +1,9 @@
 function solution(clothes){
     const map = new Map();
-    for(const [,cat] of clothes) map.set(cat, (map.get(cat)||0)+1);
-    return [...map.values()].reduce((a,b)=>a*(b+1),1)-1;
+    
+    for([,cat] of clothes){
+        map.set(cat, (map.get(cat) ||0) +1)
+    }
+    return [...map.values()]
+    .reduce((acc, cur) => acc * (cur+1),1)-1
 }
