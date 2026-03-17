d134f2c8c76744998d79cb3d32a11b776d1dc1a5
JEN023__ <165226466+Ganadalam@users.noreply.github.com>
2026-03-17 23:43:17 +0900

    [level 2] Title: H-Index, Time: 0.66 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,5 +1,6 @@
 function solution(citations){
-    citations.sort((a,b) => b-a);
-    const idx = citations.findIndex((c,i) => c < i+1)
+    citations.sort((a,b) => b-a);  //내림차순
+    const idx = citations.findIndex((c,i) => c < i+1) // 현재 논문 인용 수 < 지금까지 논문 수 
     return idx !== -1 ? idx : citations.length 
+    //깨지는 지점 O: idx, 끝까지 안 깨짐: -1(논문 all 조건 만족)
 }
